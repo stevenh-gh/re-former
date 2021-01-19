ea09b3b457baa2b0d01a5810f66a668000fc7f3f
steven <steven.h@live.com>
2021-01-18 16:05:40 -0800

    add edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,23 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+
+    if @user.update user_params
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
